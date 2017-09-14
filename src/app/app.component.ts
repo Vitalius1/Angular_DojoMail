8a855bc5bdfa8d8725f53a91635009893134753a
VitalieBraga <vitaliebraga@gmail.com>
2017-09-14 16:40:29 -0700

    Dojo mail assignment finished. Reused the My First Angular app testing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  emails = [
+    {
+      email: 'Vitalius1@yandex.com',
+      importance: false,
+      subject: 'Programing',
+      content: 'blabalbalbalbalbbabalbalablablabalbal'
+    },
+    {
+      email: 'vitaliebraga@gmail.com',
+      importance: true,
+      subject: 'Cooking',
+      content: 'blabalbalbalbalbbabalbalablablabalbal'
+    },
+    {
+      email: 'diaNA@mail.com',
+      importance: false,
+      subject: 'Party',
+      content: 'blabalbalbalbalbbabalbalablablabalbal'
+    },
+    {
+      email: 'john@smith.com',
+      importance: true,
+      subject: 'School',
+      content: 'blabalbalbalbalbbabalbalablablabalbal'
+    },
+  ]
 }
